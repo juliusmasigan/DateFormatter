f64760c1c8c155b06fbc2638e77be7d067f8cc63
Julius Masigan <juliusmasigan@gmail.com>
2015-11-12 10:52:50 +0800

    First working version.

--- dist/date-formatter.js
+++ dist/date-formatter.js
@@ -6,18 +6,24 @@ $.fn.extend({
 			var monthMap = ['january', 'february', 'march', 'april', 'may', 'june',
 				'july', 'august', 'september', 'october', 'november', 'december'];
 
-			var month = date.match(/[A-Z]+/, 'gi');
+			var month = date.match(/[A-Z]+/i, 'g');
 			month = month[0];
 
 			var ddate = date.match(/[0-9]{1,2}/, 'g');
-			ddate = ddate[0];
+			ddate = String(ddate[0]);
 
 			var year = date.match(/[0-9]{4}/, 'g');
 			year = year[0];
 
 			switch(format) {
 				case 'mm.dd.yy':
-					formattedDate = month.toLowerCase();
+					monthLower = month.toLowerCase();
+                    monthIndex = String(monthMap.indexOf(monthLower) + 1);
+                    monthPadded = "00".substring(0, 2 - monthIndex.length) + monthIndex;
+                    datePadded = "00".substring(0, 2 - ddate.length) + ddate;
+
+                    dateFormated = monthPadded+"."+datePadded+"."+year;
+                    $(v).text(dateFormated);
 					break;
 			}
 
